60f8cb72184ca76ebf951b04de68571907162475
Michael Stepniewski <infotechpro.stepniewski@gmail.com>
2014-09-04 13:17:00 -0500

    update

--- lshost.pl
+++ lshost.pl
@@ -1,16 +1,26 @@
-#!/usr/bin/perl -w
-use strict;
 use warnings;
+use strict;
 use diagnostics;
-use filehandle;
+use FileHandle;
 
-my $FH    = FileHandle->new;
-my @hosts = <$inputfh>;
+my $inputfile  = FileHandle->new;
+my $outputfile = FileHandle->new;
+my @hosts;
+
+open($inputfile, "<", "input.txt")
+        or die "cannot open < input.txt: $!";
+        @hosts = <$inputfile>;
+close ($inputfile) || warn "close failed: $!";
+
+qx(touch output.txt);
+
+open($outputfile, ">>", "output.txt")
+        or die "cannot open > output.txt: $!";
 
 foreach my $hosts (@hosts) {
 	print $outputfile $hosts."\n";
-	print "Testing domain...\n"
         $hosts = qx(host $hosts);
         print $outputfile $hosts."\n";
-	sleep (1); #Keep DNS servers happy
 }
+
+close ($outputfile) || warn "close failed: $!";
